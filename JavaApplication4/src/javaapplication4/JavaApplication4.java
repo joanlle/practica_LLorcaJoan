54e6e6c3541b7fa0f9aa0af3265e81ea44ab4b26
joanlle <146271711+joanlle@users.noreply.github.com>
2024-02-13 20:37:33 +0100

    Add files via upload

---
+++ JavaApplication4/src/javaapplication4/JavaApplication4.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication4;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author jllorca.daw1r23
+ */
+public class JavaApplication4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int numero;
+   
+     for (int compt=1; compt<=10;compt++){
+         System.out.println("Introdueix un numero");
+         Scanner teclat = new Scanner(System.in);
+         numero=teclat.nextInt();
+         
+         
+     }
+    
+    
+}
+ 
+}
+    
+
